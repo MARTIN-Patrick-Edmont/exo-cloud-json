fe48cfb4d00fbd1fba5af07ca9ee1c022db802d0
patrick <patrick.martin@limayrac.fr>
2025-02-27 23:27:39 +0100

    json mono text

--- function_app_sanitised.py
+++ function_app_sanitised.py
@@ -12,7 +12,11 @@ def exojson([BLOB_NAME]: func.InputStream):
                 f"Blob Size: {myblob.length} bytes")
     [BLOB_NAME].name.match(r".*(\.json)$")
     if re.match(r".*(.json)\Z"): # Is it a .json ?
-        try : json.load([BLOB_NAME]) # Is it a *good* json ?
+        try : BufferDict = json.load([BLOB_NAME]) # Is it a *good* json ?
+            if text = BufferDict.get("text") # is there text in the json ? (this doesn't account for the nested dictionaries a more complex json would make, or multiple text keys, but no time to develop that)
+                text = httpPOST(text,AzureTranslateAPI) #pseudocode, because, again, no time
+                BufferDict.update({"text":text}) # Overwriting with the translated text
+                BlobUpload(json.dump(BufferDict)) #Pseudocode yet again
         except: logging.error("JSON failed to decode")
     else:
         logging.error("The file submitted was not a JSON file.")
